b3b083adbe880c148c835cfc25fa9bb3aaa1ff88
Vlad <kononvlad007@gmail.com>
2026-05-13 13:31:55 +0300

    deleted unneccesary files

--- oldClient/js/btnTest.js
+++
@@ -1,8 +0,0 @@
-const myBtn = document.getElementById("btn-back-send");
-
-export const func = myBtn.addEventListener("click",  () => {
-    console.log("Button clicked");
-    fetch('/api/values')
-    .then(response => response.json())
-    .then(data => alert(data.message))
-});
